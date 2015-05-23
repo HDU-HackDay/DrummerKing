8b15e7074ca451c3095737922d27095cfcc129e9
homeway <xiaocao.grasses@gmail.com>
2015-05-23 12:16:55 +0800

    init

---
+++ client/web/lib/socket.js
@@ -0,0 +1,42 @@
+var WebSocket = function(url){
+	var callbacks = {};
+	var ws_url = url;
+	var conn;
+	this.bind = function(event_name, callback){
+		callbacks[event_name] = callbacks[event_name] || [];
+		callbacks[event_name].push(callback);
+		return this;// chainable
+	};
+	this.send = function(event_name, event_data){
+		this.conn.send( event_data );
+		return this;
+	};
+	this.connect = function() {
+		if ( typeof(MozWebSocket) == 'function' )
+			this.conn = new MozWebSocket(url);
+		else
+			this.conn = new WebSocket(url);
+		this.conn.onmessage = function(evt){
+			dispatch('message', evt.data);
+		};
+		this.conn.onclose = function(){dispatch('close',null)}
+		this.conn.onopen = function(){dispatch('open',null)}
+	};
+	this.disconnect = function() {
+		this.conn.close();
+	};
+	var dispatch = function(event_name, message){
+		var chain = callbacks[event_name];
+		if(typeof chain == 'undefined') return;
+		for(var i = 0; i < chain.length; i++){
+			chain[i]( message )
+		}
+	}
+};
+
+/*
+usage
+var Server = new WebSocket('ws://10.20.122.234:9300');
+Server.send("message", "1234" );
+*/
+
